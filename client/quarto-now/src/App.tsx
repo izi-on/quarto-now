215633a41a1e41a3c5687c6e0953d172d0547a9f
Hristo <sandevh@gmail.com>
2024-05-08 19:07:58 -0400

    tailwind

--- client/quarto-now/src/App.tsx
+++ client/quarto-now/src/App.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="bg-green-500">Hello World</div>;
+  return <div className="text-blue-600">Hello World</div>;
 }
 
 export default App;
